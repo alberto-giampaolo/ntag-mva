4a6b4b59ac2f365212c1a07b474e13d03eb8561f
Alberto Giampaolo <alberto.giamp@gmail.com>
2019-12-09 01:25:06 +0100

    NN training tweak

--- training_dnn.py
+++ training_dnn.py
@@ -1,12 +1,14 @@
 from os import environ, devnull
 environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Suppress avalanche of tensorflow info outputs
+
 import sys
 stderr = sys.stderr
 sys.stderr = open(devnull, 'w') # Suppress Keras info outputs
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 sys.stderr = stderr
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -15,11 +17,13 @@ from roc import plot_ROC_sigle, plot_ROC_sigle_gen
 
 
 N10TH = 7
-NFILES = 1
-STARTFILE = 1
-NEPOCHS = 10
-BATCH_SIZE = 32
-model_name = "NN\\batch_size\\NN22_n%i_%iepoch_%ibatch_%i"%(N10TH, NEPOCHS,BATCH_SIZE, NFILES)
+NFILES = 50
+STARTFILE = 0
+NEPOCHS = 100
+BATCH_SIZE = 256
+NLAYERS = 1
+NNODES = 50
+model_name = "NN\\epochs\\NN22_n%i_%iepoch_%ibatch_%ix%i_%i"%(N10TH, NEPOCHS,BATCH_SIZE, NLAYERS,NNODES,NFILES)
 
 if __name__ == '__main__':
     #x_test, x_train, y_test, y_train = load_dset(N10TH, NFILES)
@@ -28,21 +32,24 @@ if __name__ == '__main__':
 
     # Define neutron tagging model
     ntag_model = Sequential()
-    ntag_model.add(Dense(22, activation='relu', input_dim=22))
+    ntag_model.add(Dense(NNODES, activation='relu', input_dim=22))
     ntag_model.add(Dropout(0.5))
-    ntag_model.add(Dense(22, activation='relu'))
-    #ntag_model.add(Dropout(0.5))
+    for layer in range(NLAYERS-1):
+        ntag_model.add(Dense(NNODES, activation='relu'))
+        ntag_model.add(Dropout(0.5))
     ntag_model.add(Dense(1, activation='sigmoid'))
     #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=False)
-    ntag_model.compile(loss='mean_squared_logarithmic_error', optimizer='adam', metrics=['accuracy'])
+    adam = Adam(learning_rate=0.001*BATCH_SIZE/64., beta_1=0.9, beta_2=0.999, amsgrad=False)
+    ntag_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # Train
+    print("Starting training...")
     start_time = time.time()
     #ntag_model.fit(x_train,y_train, epochs=5, verbose=1)
-    ntag_history = ntag_model.fit_generator(train_gen, validation_data=test_gen,epochs=NEPOCHS,verbose=1,use_multiprocessing=True, workers=3)
+    ntag_history = ntag_model.fit_generator(train_gen, validation_data=test_gen,epochs=NEPOCHS,verbose=1,use_multiprocessing=True, workers=1)
     end_time = time.time()
     training_dt = end_time-start_time
-    print("Trained model in %f seconds" % training_dt)
+    print("Trained model in %0.2f seconds" % training_dt)
 
     # Save model and history object
     save_model(ntag_model, model_name, hist=ntag_history)
@@ -53,10 +60,9 @@ if __name__ == '__main__':
     y_test, _ = load_dset(N10TH, NFILES, mode='y', start_file=STARTFILE)
     x_test_gen= ntagGenerator(N10th=N10TH, num_files=NFILES, test_frac=0.25, batch_size=BATCH_SIZE, train=False, mode='x', start_file=STARTFILE)
     plot_ROC_sigle_gen(x_test_gen, y_test, ntag_model, model_name, N10TH)
-    print("All done!")
 
     # Plot loss vs epoch
-    print("Plotting loss evolution")
+    print("Plotting loss history")
     plt.plot(ntag_history.history['loss'], label='Training')
     plt.plot(ntag_history.history['val_loss'], label='Testing', color='tab:red')
     plt.ylabel('Loss')
@@ -64,3 +70,5 @@ if __name__ == '__main__':
     plt.yscale('log')
     plt.legend()
     plt.show()
+
+    print("All done!")
