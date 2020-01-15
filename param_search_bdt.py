48a1df22cee135d9d97ceeb3c92338f24867f2a5
Alberto Giampaolo <alberto.giamp@gmail.com>
2020-01-15 14:22:29 +0100

    Completed hyperparameter search code

--- param_search_bdt.py
+++ param_search_bdt.py
@@ -6,20 +6,20 @@ from xgboost import XGBClassifier
 with warnings.catch_warnings():  # Clean up output
     warnings.filterwarnings("ignore",category=FutureWarning)
     from load_ntag import load_dset, save_model
-print('Ready') 
-
 
 N10TH = 7
-NFILES = 10
-NTHREADS = 4
+NFILES = 50
+NTHREADS = -1 # Automatically make use of max cores (test)
 
+# Directory in which to save models and parameters (should exist inside the "models" directory)
+grid_location = 'BDT/grid_0_extend_066_fast/'
 
 try:
     param_file = sys.argv[1]
-    model_name = sys.argv[2]
 except IndexError:
-    raise ValueError("Usage: python3 param_search_bdt paramfile modelname")
+    raise ValueError("Usage: python3 param_search_bdt.py paramfile")
 
+model_name = param_file.split("/")[-1].split('.')[-2] # model has same name as param file
 
 # Load dataset
 print("Loading dataset...")
@@ -29,26 +29,27 @@ print("Training BDT")
 # Define neutron tagging model and start training
 with open(param_file, 'rb') as fl:
     params = load(fl)
+    print('Loaded parameter file: '+param_file)
 ntag_model = XGBClassifier(learning_rate=params['learning_rate'],
-                          n_estimators=int(params['n_estimators']),
+                          #n_estimators=int(params['n_estimators']),
+                          n_estimators=6000,
+                          early_stopping_rounds=500,
                           subsample=params['subsample'],
                           verbosity=1,
-                          nthread=NTHREADS) 
-
+                          nthread=NTHREADS)
+print('Built BDT with these hyperparameters:')
+print(params)
 
 start_time = time.time()
 ntag_model.fit(x_train,y_train,
               eval_set=[(x_train,y_train),(x_test,y_test)],
-              eval_metric='mae')
+              eval_metric=['mae','auc'])
 end_time = time.time()
 training_dt = end_time-start_time
-print("Trained model in %f seconds" % training_dt)
+print("Trained model with ID "+model_name+" in %f seconds" % training_dt)
 
 # Save model
 print("Saving model to disk...")
-save_model(ntag_model, model_name)
-
-
-# Plot ROC curve
+save_model(ntag_model, model_name, subloc=grid_location+'models/')
 
 print("All done!")
--- param_search_nn.py
+++ param_search_nn.py
@@ -1,68 +1,75 @@
-
+import time
+import sys
+from joblib import load
+from os import environ
+environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Suppress tensorflow info outputs
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import SGD, Adam
-import numpy as np
-import matplotlib.pyplot as plt
-import time
-from matplotlib import use as mpl_use
+from keras.optimizers import SGD, Adam, Nadam, Adadelta
+from keras import metrics
 from load_ntag import load_dset, save_model, ntagGenerator
-from roc import plot_ROC_sigle, plot_ROC_sigle_gen
-
 
 N10TH = 7
-NFILES = 10
+NFILES = 200
 STARTFILE = 0
-NEPOCHS = 2
 BATCH_SIZE = 256
-NLAYERS = 1
-NNODES = 50
-model_name = "polui_tests/NN22_n%i_%iepoch_%ibatch_%ix%i_%i"%(N10TH, NEPOCHS,BATCH_SIZE, NLAYERS,NNODES,NFILES)
+
+# Directory in which to save models and parameters (should exist inside the "models" directory)
+grid_location = 'NN/test_search/'
+
+try:
+    param_file = sys.argv[1]
+except IndexError:
+    raise ValueError("Usage: python3 param_search_nn.py paramfile")
+
+model_name = param_file.split("/")[-1].split('.')[-2] # model has same name as param file
+
 
 if __name__ == '__main__':
+
+    # Initialize model according to parameter file
+    with open(param_file, 'rb') as fl:
+        params = load(fl)
+        print('Loaded parameter file: '+param_file)
+
+    ntag_model = Sequential()
+    ntag_model.add(Dense(params['width'], activation='relu', input_dim=22))
+    ntag_model.add(Dropout(params['dropout']))
+    for layer in range(params['depth']-1):
+        ntag_model.add(Dense(params['width'], activation='relu'))
+        ntag_model.add(Dropout(params['dropout']))
+    ntag_model.add(Dense(1, activation='sigmoid'))
+
+    if params['optimizer']=='sgd':
+        opt = SGD(learning_rate=params['learning_rate'], decay=1e-6, momentum=0.9, nesterov=False)
+    elif params['optimizer']=='adam':
+        opt = Adam(learning_rate=params['learning_rate'], beta_1=0.9, beta_2=0.999, amsgrad=False) # lr = 0.001*BATCH_SIZE/64.
+    elif params['optimizer']=='nadam':
+        opt = Nadam(learning_rate=params['learning_rate'], beta_1=0.9, beta_2=0.999)
+    elif params['optimizer']=='adadelta':
+        opt = Adadelta() # Keep Adadelta with default settings
+    else:
+        raise ValueError("Invalid optimizer parameter: " + params['optimizer'])
+    ntag_model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy',metrics.AUC()])
+    print('Built NN with these hyperparameters:')
+    print(params)
+
     #x_test, x_train, y_test, y_train = load_dset(N10TH, NFILES)
+    print("Initializing dataset generators...")
     train_gen = ntagGenerator(N10th=N10TH, num_files=NFILES, test_frac=0.25, batch_size=BATCH_SIZE, train=True, start_file=STARTFILE)
     test_gen  = ntagGenerator(N10th=N10TH, num_files=NFILES, test_frac=0.25, batch_size=BATCH_SIZE, train=False, start_file=STARTFILE)
-
-    # Define neutron tagging model
-    ntag_model = Sequential()
-    ntag_model.add(Dense(NNODES, activation='relu', input_dim=22))
-    ntag_model.add(Dropout(0.5))
-    for layer in range(NLAYERS-1):
-        ntag_model.add(Dense(NNODES, activation='relu'))
-        ntag_model.add(Dropout(0.5))
-    ntag_model.add(Dense(1, activation='sigmoid'))
-    #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=False)
-    adam = Adam(learning_rate=0.001*BATCH_SIZE/64., beta_1=0.9, beta_2=0.999, amsgrad=False)
-    ntag_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print("Generators initialized")
 
     # Train
     print("Starting training...")
     start_time = time.time()
     #ntag_model.fit(x_train,y_train, epochs=5, verbose=1)
-    ntag_history = ntag_model.fit_generator(train_gen, validation_data=test_gen,epochs=NEPOCHS,verbose=1,use_multiprocessing=False, workers=0)
+    ntag_history = ntag_model.fit_generator(train_gen, validation_data=test_gen,epochs=params['epochs'],verbose=1,use_multiprocessing=False, workers=0)
     end_time = time.time()
     training_dt = end_time-start_time
-    print("Trained model in %0.2f seconds" % training_dt)
+    print("Trained model with ID "+model_name+" in %0.2f seconds" % training_dt)
 
     # Save model and history object
     save_model(ntag_model, model_name, hist=ntag_history)
-    print("Saved model to disk.")
-
-    # Plot ROC curve
-    print("Plotting performance (ROC)")
-    y_test, _ = load_dset(N10TH, NFILES, mode='y', start_file=STARTFILE)
-    x_test_gen= ntagGenerator(N10th=N10TH, num_files=NFILES, test_frac=0.25, batch_size=BATCH_SIZE, train=False, mode='x', start_file=STARTFILE)
-    plot_ROC_sigle_gen(x_test_gen, y_test, ntag_model, model_name, N10TH)
-
-    # Plot loss vs epoch
-    print("Plotting loss history")
-    plt.plot(ntag_history.history['loss'], label='Training')
-    plt.plot(ntag_history.history['val_loss'], label='Testing', color='tab:red')
-    plt.ylabel('Loss')
-    plt.xlabel('Training epoch')
-    plt.yscale('log')
-    plt.legend()
-    plt.show()
-
+    print("Saved model and training history to disk.")
     print("All done!")
