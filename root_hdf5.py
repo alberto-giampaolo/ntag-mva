0b825e57bf9afcae481c175e7aba55ae1bd3e246
Alberto Giampaolo <alberto.giamp@gmail.com>
2020-01-15 14:39:25 +0100

    Deleted superfluous files

--- root_hdf5.py
+++
@@ -1,9 +0,0 @@
-from rootpy.root2hdf5 import root2hdf5
-
-start, stop = 50, 100
-
-rootfile_ins = ["/media/alberto/KINGSTON/Data/flat/shift0/%03i.root"%i for i in range(start,stop)]
-hdf5file_outs = ["/media/alberto/KINGSTON/Data/hdf5_flat/shift0/%03i.hdf5"%i for i in range(start,stop)]
-
-for rin, hout in zip(rootfile_ins, hdf5file_outs):
-    root2hdf5(rin,hout,entries=10000, show_progress=True)
